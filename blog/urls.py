17eb8f46387ba88a5768f9f0d14756df6663a4f7
Marius Hullin <marius.hullin@gmail.com>
2020-02-14 11:52:46 -0800

    Initial modifs

--- blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, re_path
-from . import views
-
-urlpatterns = [
-    path('date', views.date_actuelle),
-    path('addition/<int:nombre1>/<int:nombre2>/', views.addition)
-]
